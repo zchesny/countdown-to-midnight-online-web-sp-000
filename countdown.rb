92fe83c8fcba1be9641e5d92d2a63e592e252f45
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 17:59:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,11 +11,7 @@ end
 def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
-<<<<<<< HEAD
     number -= 1
-=======
-    number == 1
->>>>>>> 4eede8725012340f2039a170d337a36f8edaf39c
     sleep(1)
   end
   "HAPPY NEW YEAR!"
